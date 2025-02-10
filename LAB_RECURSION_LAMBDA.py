f7e2d5f4326f7d27ab1408d44a111d88c1a4264d
abdulrahman aidarous <abdulrahmanaid.21@gmail.com>
2025-02-10 12:20:59 +0300

    Solution for LAB_RECURISON_LAMBDA

--- LAB_RECURSION_LAMBDA.py
+++ LAB_RECURSION_LAMBDA.py
@@ -4,17 +4,14 @@ def count_vowels(text:str)->int:
             text(str):this the text that will be count how many vowels in
        return
             int:it will be return the count of vowels in pharse or or word in intger          '''
+    t='aeiou'
     if text=="":
         return 0
     else:
-        if text[0].lower()=='a' or text[0].lower()=='e' or text[0].lower()=='i' or text[0].lower()=='o' or text[0].lower()=='u':
-            return 1+count_vowels(text[1:]) 
-        else:
-             return 0+count_vowels(text[1:])
+        return 1+count_vowels(text[1:]) if text[0].lower() in t else 0+count_vowels(text[1:])
 print(count_vowels("I Love python"))
 
 
 list_of_numbers=[40,35, 10, 15, 20]
 new_list=list(map(lambda num:num*num,list_of_numbers))
 print(new_list)
-
