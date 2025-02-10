6c9a89509754e9e33d8eab0f94382c8b59b7bff1
mohamad <mo.alharbi077@gmail.com>
2025-02-10 12:55:14 +0300

    filnal commit

--- lab_recursion.py
+++ lab_recursion.py
@@ -1,3 +1,4 @@
+# 1 function takes a word and returns a number of vowels
 def HowManyVowels(word:str) -> int:
     count = 0
     if len(word) == 0:
@@ -6,9 +7,16 @@ def HowManyVowels(word:str) -> int:
     if word[0].lower() in vowels:
         count +=1
     return count + HowManyVowels(word[1:]) 
-print(HowManyVowels("I love python"))
 
-l = [1,2,3,4,5]
-l2 = list(map(lambda lst:lst*2,l))
-print(l)
-print(l2)
+#test the function
+while(True):
+    phrase = input("Enter a phrase (0 to exit): ")
+    if phrase == '0':break
+    print(f"'{phrase}' continains {HowManyVowels(phrase)} vowels. ")
+
+
+# 2 create new multipled list 
+numbers = [40,35, 10, 15, 20]
+multiplied_list = list(map(lambda lst:lst*lst,numbers))
+print(f"original list is : {numbers}")
+print(f"muliplied by itself : {multiplied_list}")
